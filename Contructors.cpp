c8d2399ffcd45edc9ad0fe38e6df6bb57c5a0b0e
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-04-08 19:31:36 +0200

    Add notes on constructors and OOP function definitions

---
+++ Contructors.cpp
@@ -0,0 +1,13 @@
+// Notes on CONSTRUCTORS:
+/*
+a constructor is special C++ that is automatically called when an object of a class is created.
+When creating a construnctor, the name of the constructor must be the same as the class name.
+Constructors do not have a return type, not even void. They are used to initialize the object when it is created.
+class MyClass{
+    public:
+        MyClass(){ // Constructor
+            // Initialization code here
+        }
+};
+A constructor is always public and it has no return type
+They ca also take parameter, they are always.aligna*/
--- Notes_On_OOP.cpp
+++ Notes_On_OOP.cpp
@@ -9,6 +9,12 @@ Classes can have functions or methods defined within them
  These methods can be used to manipulate the data within the class or perform actions related to the class.
 
 
- Defining a 
+ Definine a function outside the clas definition, you have to declare it inside
+ the class, folloowed the schop resoltion :: operator and then the function name.
 
+class Cat{
+}
+int Car::speed(int speed){
+    return speed;
+}
 */
