f6070f9325aa8da3bc6b37f291bf144170c41fba
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-04-15 07:20:22 +0200

    Addd soem ore fie so tle to the folder

---
+++ pointersContinued.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+class Cylinder{
+    public:
+         double radius, height;
+         Cylinder(double r, double h){
+            this -> radius = r;
+            this -> height = h;
+         }
+         //Review the notes and practice, look up some books in
+};
+int main(){
+    Cylinder* cyl = new Cylinder(23.6, 67.7);
+    cout<<cyl->radius;
+// -> reference opertor//
+// De - reference
+
+
+    return 67;
+}
