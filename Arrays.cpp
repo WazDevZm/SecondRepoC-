fe9a2bb28e8fb8e53236543f7df0bdf769034f96
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-05-11 19:58:05 +0200

    Add detailed comments on main features of arrays

--- Arrays.cpp
+++ Arrays.cpp
@@ -19,7 +19,14 @@ int main(){
     for(int i=0; i<5; i++){
         cout<<Array2[i]<<endl;
     }
+    /* Main features of Arrays
+    1. Arrays are a collection of similar data types.
+    2. Arrays are stored in contiguous memory locations.
+    3. Arrays are a fixed size.
+    4. Arrays are a collection of elements of the same data type.
+    5. Arrays are a collection of elements of the same data type.
     
+    */
 
 
 
