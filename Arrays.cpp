3a8a47689c226f3fbd08d22d1411d089d38bd7c2
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-05-11 20:17:52 +0200

    added more files to the stack

--- Arrays.cpp
+++ Arrays.cpp
@@ -25,21 +25,30 @@ int main(){
     3. Arrays are a fixed size.
     4. Arrays are a collection of elements of the same data type.
     5. Arrays are a collection of elements of the same data type.
-    
+
     */
 
+    // finding the avrage of elements in an array
+    int Array4[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int length = sizeof(Array4)/sizeof(Array4[0]);
+    float sum = 0;
+    float average;
 
+    //finding the sum using the for each lopp
+    for(int a :  Array4){
+        sum+= a;
+    }
+    average = sum/length;
+    cout << "The average of the array is: " << average << endl;
+    // program to find the largest element in an array
+    int Array5[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int largest = Array5[0];
 
-
-
-
-
-
-
-
-
-
-
+    for (int x : Array5) {
+        if (x > largest) {
+            largest = x;
+        }
+    }
 
     return 9;
 }
