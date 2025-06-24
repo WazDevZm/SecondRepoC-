225310ab01cb3b7e7becafb85f8344616bd5f2d3
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-06-25 00:15:52 +0200

    Refactor code structure for improved readability and maintainability

--- SESSIONAL PREP/Day1/more_code.cpp
+++ SESSIONAL PREP/Day1/more_code.cpp
@@ -3,45 +3,82 @@
 using namespace std;
 
 
-
 class Fruit {
     public:
-    string name;
-    int* next = NULL;
-    int* prev = NULL;
+    string firstName;
+    string newName;
+    Fruit* next = NULL;
+    Fruit* prev = NULL;
    // int* next= nullptr;// 0 can also be used.
+   Fruit(string fruitName){
+    this->firstName = fruitName;
+    this->newName = "";
+    // static and instance
+   }
 
-    void LinkedList(int firstNode){};
-
-
-
-
-    void RemoveFirst(int firstNode);
-
-
-
-    void RemoveLast(int firstNode);
+    static void printList();
 
 
 
 
-    void RemoveInner(int firstNode, string fruitName);// this is a fnctio prototyp. dclaration
+    static bool RemoveFirst(Fruit* firstNode);
+
+
+
+    static bool RemoveLast(Fruit* firstNode);
+
+
+
+
+    static bool RemoveInner(Fruit*firstNode, string fruitName);// this is a fnctio prototyp. dclaration
+
 
 
     
-    
-    void Search(int firstNode, string fruitName);
-
-
-    
+    static void SearchList(Fruit* firstNode, string fruitName);
+    //static void printlist();
+    static void push (Fruit* newNode);
 
 };
 
+Fruit* head = nullptr;
+
+void Fruit::push(Fruit*newNode){
+        if (head == NULL){
+            
+            return;
+            // It is up tot the main to maintain the firstnode
+        }else{
+        
+            Fruit* temp = head;
+            while(temp-> next != NULL){
+                temp = temp-> next;
+            }
+            // link newNode to temp
+            temp-> next = newNode;
+            newNode -> prev = temp;
+        }
+ };
+void Fruit::printList(){
+    Fruit* temp = head;
+    while (temp != nullptr)
+    {
+        cout<< temp->firstName<<" ";
+        temp = temp ->next;
+    }
+   
+}
+void Fruit::SearchList(string fn);
+
 int main() {
 
-
-
-
+    Fruit::push(new Fruit("Mango"));
+    Fruit::push(new Fruit("Banana"));
+    Fruit::push(new Fruit("Orange"));
+    Fruit::push(new Fruit("Lemon"));
+    Fruit::push(new Fruit("Guava"));
+    Fruit::printList();
+    cout<<Fruit::SearchList("Guava");
 
 
     return 5;
